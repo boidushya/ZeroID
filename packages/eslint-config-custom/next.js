aefef77b241b2e3649dfc3a98f6ac75b4080e335
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2023-10-29 13:15:27 +0530

    fix: eslint config and tsconfig path

--- packages/eslint-config-custom/next.js
+++ packages/eslint-config-custom/next.js
@@ -12,31 +12,5 @@ const project = resolve(process.cwd(), "tsconfig.json");
  */
 
 module.exports = {
-  extends: [
-    "@vercel/style-guide/eslint/node",
-    "@vercel/style-guide/eslint/browser",
-    "@vercel/style-guide/eslint/typescript",
-    "@vercel/style-guide/eslint/react",
-    "@vercel/style-guide/eslint/next",
-    "eslint-config-turbo",
-  ].map(require.resolve),
-  parserOptions: {
-    project,
-  },
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
-    },
-  },
-  ignorePatterns: ["node_modules/", "dist/"],
-  // add rules configurations here
-  rules: {
-    "import/no-default-export": "off",
-  },
+  extends: "next/core-web-vitals",
 };
