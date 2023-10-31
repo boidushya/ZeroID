cf9967f6236bd5505b8c84c5f7417be2f80a5c24
Saurabh Patil <55076843+Saurabhpatil-dev@users.noreply.github.com>
2023-10-31 19:10:43 +0530

    :safety_vest: final function for interaction added

--- contracts/mid/src/main.ts
+++ contracts/mid/src/main.ts
@@ -64,3 +64,33 @@ let tx = await Mina.transaction(feePayer, () => {
 await tx.prove();
 await tx.sign([feePayerKey, zkappKey]).send();
 console.log('Contract Deployed....');
+
+// To add a new account
+try {
+  let [updatedCommitment, qrString] = createNewRoot(a2Adnum, a2Salt);
+  let tx2 = await Mina.transaction(feePayer, () => {
+    minaID.updateRoot(feePayerKey, updatedCommitment);
+  });
+  await tx2.prove();
+  await tx2.sign([feePayerKey, zkappKey]).send();
+  console.log('QRString ......', qrString.toString());
+} catch (e: any) {
+  console.log(e.message);
+}
+
+// To verify the account
+async function verify(qrString: Field, index: bigint) {
+  try {
+    let w = Tree.getWitness(index);
+    let witness = new MyMerkleWitness(w);
+    const verified = minaID.verifiedOrNot(feePayer, qrString, witness);
+
+    if (verified) {
+      console.log(`Dude is verified Mina Partner !!!`);
+    } else {
+      console.log(`Hahaha!! Impposter Alert`);
+    }
+  } catch (e: any) {
+    console.log(e.message);
+  }
+}
